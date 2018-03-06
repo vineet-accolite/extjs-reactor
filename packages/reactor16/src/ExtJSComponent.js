7d6a0330d89309ac5ae60f06c2d4bb2da1305456
mgusmano <mgusmano@yahoo.com>
2018-03-06 00:22:13 -0500

    cleanup from previous pull

--- packages/reactor16/src/ExtJSComponent.js
+++ packages/reactor16/src/ExtJSComponent.js
@@ -11,7 +11,6 @@ export class ExtJSComponent extends React.Component {
   constructor(props) {
     super(props)
     this.props = props
-//    this.xtype = this.getxtype()
     var config = {}
     config.xtype = this.xtype
     if (this.isRootContainer()) {
@@ -72,6 +71,7 @@ export class ExtJSComponent extends React.Component {
     return null
   }
 
+  //should we use this??
   _renderRootComponent(renderToDOMNode, config) {
     defaults(config, {
         height: '100%',
@@ -106,6 +106,7 @@ export class ExtJSComponent extends React.Component {
       }
   }
 
+  //should we use this?
   /**
    * Creates an Ext JS component config from react element props
    * @private
@@ -157,6 +158,7 @@ export class ExtJSComponent extends React.Component {
       return config;
   }
 
+  //should we use this??
   /**
    * Determines whether a child element corresponds to a config or a container item based on the presence of a rel config or
    * matching other known relationships
@@ -479,147 +481,4 @@ export class ExtJSComponent extends React.Component {
       // When tab to the left of the active tab is removed, the left-most tab would always be selected as the tabs to the right are reinserted
       if (CLASS_CACHE.TabPanel && this.cmp instanceof CLASS_CACHE.TabPanel) return true;
   }
-
-
-
-
-
 }
-
-
-//		this.renderChildren = this.renderChildren.bind(this)
-
-// 	renderChildren() {
-// 		return React.Children.map(this.props.children, child => {
-// 			if (child == null) {
-// 				return null
-// 			}
-// 			// if(child.xtype == undefined) {
-// 			// 	return null
-// 			// }
-// //			l('child', child)
-// 			return React.cloneElement(child, {
-// //				theParentCmp: this.state.cmp
-// 			})
-// 		})
-// 	}
-
-
-		// var children = this.renderChildren()
-		// var childrenLength 
-		// if (children == undefined) {
-		// 	childrenLength = 0
-		// 	children = []
-		// }
-		// else {
-		// 	childrenLength = children.length
-		// }
-		// var names = ''
-		// for (var child of children) {
-		// 	//console.log(child)
-		// 	if (child.type.name) {
-		// 	names = names + `${child.type.name}(${child.props.d},${child.type.source}), `
-		// 	}
-		// }
-		// l(`render ${this.xtype}, ${childrenLength} items`,names)
-		// var root = document.getElementsByClassName('x-viewport-body-el')[0]
-		// return ReactDOM.createPortal(
-		// 	children,
-		// 	root,
-		// )
-
-		// // this.htmlDiv = document.createElement("div")
-		// // return ReactDOM.createPortal(
-		// // 	children,
-		// // 	this.htmlDiv,
-		// // )
-		// //var root = this.cmp
-
-
-		// if (childrenLength == 0) {
-		// 	return(null)
-		// }
-		// else {
-		// 	return(children)
-		// }
-
-
-// getChild(child) {
-// 	if (child.type  == 'div' || 
-// 	child.type == 'h1' ||
-// 	child.type == 'h2' ||
-// 	child.type == 'li' ||
-// 	child.type == 'p' ||
-// 	child.type == 'ul' ||
-// 	child.type == 'span' ||
-// 	child.type == 'a') {
-// 		this.html = this.html + ' ' + `<` + child.type + ''
-// 		for (var prop in child.props) {
-// 			if (prop != 'children') {
-// 				if (prop == 'style') {
-// 					//
-// 					var styleTagContent = JSON.stringify(child.props[prop],null,'')
-// 					.replace(/"/g,'')
-// 					.replace(/,/g,';')
-// 					.replace(/\}/g, ';') 
-// 					.replace(/\{/g, '')  
-
-// // //debugger
-
-
-// // 						var s = JSON.stringify(child.props[prop])
-// // 						var t = s.replace("{", "")
-// // 						var u = t.replace("}", ";")
-// // 						var v  = u.replace(/"/g,"")
-// // 						var w = v.replace(",", ";")
-// // 						//var v = u.replace('/"', '')
-
-// 					//debugger
-// 					//for (styleProp of child.props[prop]) {
-
-// 						this.html = this.html + ` ${prop}='${styleTagContent}'`
-
-// 						//this.html = this.html + ` ${prop}='${child.props[prop][styleProp]}'`
-// 					//}
-// 					//debugger
-// 				}
-
-// 				else if (prop == 'className') {
-// 					this.html = this.html + ` class='${child.props[prop]}'`
-// 				}
-// 				else {
-// 					this.html = this.html + ` ${prop}='${child.props[prop]}'`
-// 				}
-// 			}
-// 		}
-// 		if (typeof child.props.children == 'string') {
-// 			this.html = this.html + '>' + child.props.children
-// 			this.html = this.html + `\</` + child.type + '>\n'
-// 		}
-// 		else {
-// 			this.html = this.html + '>' + `\<` + child.type + '>'
-// 			this.getThem(child.props.children)
-// 			this.html = this.html + `\</` + child.type + '>\n'
-// 		}
-// 	}
-// }
-
-// getThem(children) {
-// 	if (children != null) {
-// 		if (children.length == undefined) {
-// 			var child = children
-// //				l('child', child.type)
-// 			if (child != undefined) {
-// 				this.getChild(child)
-// 			}
-// 		}
-// 		else {
-// 			for (var child of children) {
-// //					l('child', child.type)
-// 				if (child != undefined) {
-// 					this.getChild(child)
-// 				}
-// 			}
-// 		}
-// 	}
-// }
