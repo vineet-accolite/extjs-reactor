13f08f956d3f7b2dd1926286a2a2581bd847bc39
simran-git <simran.kaur@accolitelabs.com>
2018-04-11 05:52:33 +0530

    Added Initial Folders for Reactor Test - reactor-tests15 and reactor-tests16

---
+++ packages/reactor-tests16/src/tests/html/HTMLInExtReactComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from '@extjs/ext-react';
+
+export default function HTMLInExtReactComponent() {
+    return (
+        <Container id="container">
+            <div id="html">test</div>
+        </Container>
+    )
+}
